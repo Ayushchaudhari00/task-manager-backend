b76d8e00f45853c8bc544ceb8913545c9edf536f
Ayush chaudhari <163347553+Ayushchaudhari00@users.noreply.github.com>
2026-02-01 21:56:24 +0530

    Add method to find tasks by user ID in TaskRepo

--- src/main/java/TaskManager/TaskManager/repository/TaskRepo.java
+++ src/main/java/TaskManager/TaskManager/repository/TaskRepo.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface TaskRepo extends JpaRepository<Task,Long> {
-    List<Task> findByUserEmail(String email);
+public interface TaskRepo extends JpaRepository<Task, Long> {
+    List<Task> findByUserId(Long userId); 
+    List<Task> findByUserEmail(String email); 
 }
